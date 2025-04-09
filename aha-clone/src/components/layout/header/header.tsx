3f62ba8ecc7ce1ed724b16d84a6ee5cae4941dcc
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-04-09 12:28:33 +0530

    tedting

--- aha-clone/src/components/layout/header/header.tsx
+++ aha-clone/src/components/layout/header/header.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const header = () => {
-  return (
-    <header>
-      <div>Header</div>
-    </header>
-  );
+  return <header>{/* <div>Header</div> */}</header>;
 };
 
 export default header;
